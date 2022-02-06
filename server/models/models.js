38dee5b24cfcba76db1bcfd88863f784e3d0bf7e
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-06 19:50:30 +0300

    create models.js

--- server/models/models.js
+++ server/models/models.js
@@ -14,6 +14,10 @@ const Backet = sequelize.define('user', {
 const BacketDevice = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
+const Rating = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull:false},
+})
 const Device = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull:false},
@@ -29,7 +33,58 @@ const Brand = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull:false},
 })
-const Brand = sequelize.define('user', {
+const DeviceInfo = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, allowNull:false},
+    name: {type: DataTypes.STRING, allowNull:false},
+    description: {type: DataTypes.STRING, allowNull:false},
 })
+
+const TypeBrand = sequelize.define('type brand', {
+    id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement: true}
+})
+
+User.hasOne(Backet)
+Backet.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Backet.hasMany(BacketDevice)
+BacketDevice.belongsTo(Backet)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(BacketDevice)
+BacketDevice.belongsTo(Device)
+
+Device.hasMany(DeviceInfo)
+BacketDevice.belongsTo(Device)
+
+Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through: TypeBrand})
+Brand.belongsToMany(Type, {through:TypeBrand})
+
+
+module.exports = {
+    User,
+    Backet,
+    BacketDevice,
+    Device,
+    Type,
+    Brand,
+    Rating,
+    TypeBrand,
+    DeviceInfo
+}
