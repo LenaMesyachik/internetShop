d06cb353a7cc0b20abc429a66ab8868fc6567df2
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-09 22:40:56 +0300

    add routing

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Switch, Route, Redirect} from "react-router-dom";
+import {authRoutes} from "./routes";
 
 const AppRouter = () => {
+    const isAuth = false
     return (
-        <div>
-
-        </div>
+        <Switch>
+            { isAuth ===true && authRoutes.map(({path, Component})=>
+            <Route key={path} path={path} component={Component} exact/>
+            )}
+        </Switch>
     );
 };
 
