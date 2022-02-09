7bc9463db602f94a82aedabf9d91f6359f532049
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-09 20:37:39 +0300

    authMiddleware.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,9 @@ const ApiError = require('../error/ApiError')
 const bcrypt = require('bcrypt')
 const {User, Basket} = require('../models/models')
 const  jwt = require('jsonwebtoken')
-const  generateJwt =
+const  generateJwt = (id, email, role) => {
+     return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+}
 class UserController {
     async registration (request, response, next) {
 const {email, password, role} = request.body
@@ -17,13 +19,23 @@ const {email, password, role} = request.body
         const hashPassword  = await bcrypt.getRounds(password, 5)
         const user = await  User.create({email, role, password:hashPassword})
         const basket = await Basket.create({userID: user.id})
-        const token = jwt.sign({id:user.id, email: user.email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
-        return token
+        const token = generateJwt(user.id, user.email, user.role)
+        return response.json({token})
     }
 
 
-    async login (request, response) {
-
+    async login (request, response, next) {
+const {email, password} = request.body
+        const user  = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('пользователь не найден'))
+        }
+        let  comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('пользователь не найден'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return response.json({token})
 }
 
     async check (request, response, next) {
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,5 +1,5 @@
 const Router = require('express')
-const router = new Router
+const router = new Router()
 const deviceController = require('../controllers/deviceController')
 
 
