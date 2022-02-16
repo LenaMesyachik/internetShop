e1f6c62e92e6455419359c5034899824724ee13b
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-16 18:50:47 +0300

    create client

--- server/middleware/ErrorHandlingMiddleWare.js
+++
@@ -1,7 +0,0 @@
-const ApiError = require('../error/ApiError');
-module.exports =function (err, request, response, next) {
-    if (err instanceof ApiError) {
-        return response.status(err.status).json({message:err.message})
-    }
-    return response.status(500).json({message:{message:"непредвиденная ошибка"}})
-}
