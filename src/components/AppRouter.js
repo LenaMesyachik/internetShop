546398c20d46ec3e8c6f6bf3e35937207850b18a
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-02-16 19:25:28 +0300

    add routing

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {Routes, Route, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {authRoutes, publicRoutes} from "./routes";
 
 const AppRouter = () => {
     const isAuth = false
     return (
-       // <Route path='/welcome' element={<Home/>} />
-        <Routes >
-            { isAuth ===true && authRoutes.map(({path, Component})=>
-            <Route key={path} path={path} component={Component} exact/>
-            )}
-            { publicRoutes.map(({path, Component})=>
-                <Route key={path} path={path} component={Component} exact/>
-            )}
-        </Routes >
+        // <Route path='/welcome' element={<Home/>} />
+
+            <Routes>
+                {isAuth === true && authRoutes.map(({path, Component}) =>
+                    <Route key={path} path={path} element={Component} exact/>
+                )}
+                {publicRoutes.map(({path, Component}) =>
+                    <Route key={path} path={path} element={Component} exact/>
+                )}
+            </Routes>
+
     );
 };
 
